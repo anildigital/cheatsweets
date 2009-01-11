0a399b11510b0f4991de8d302bd0aaf625a2d4e7
Anil Wadghule <anildigital@gmail.com>
2009-01-12 01:49:06 +0530

    All my changes in autcomplete branch

--- app.rb
+++ app.rb
@@ -9,16 +9,36 @@ get "/" do
   cheats_size = $cheats_list.size
   current_cheat = $cheats_list[rand(cheats_size)]
 
-  @cookie = request.cookies["thing"]
+  @cookie = request.cookies["last_cheat"]
+
+  if current_cheat == @cookie
+    redirect "/"
+  end
 
   @cheats =  (`cheat #{current_cheat}`).split("\n")
 
   # Reset the cookie
-  set_cookie("thing", current_cheat)
+  set_cookie("last_cheat", current_cheat)
   erb :index
+
 end
 
 
+get "/query/:cheat" do
+
+  current_cheat = params[:cheat]
+  @cookie = request.cookies["last_cheat"]
+
+  @cheats =  (`cheat #{current_cheat}`).split("\n")
+
+  # Reset the cookie
+  set_cookie("last_cheat", current_cheat)
+  erb :index
+
+end
+
+
+#Exception handing for production environment
 configure :production do
 
   not_found do
