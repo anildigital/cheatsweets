98ef30c864000617d087730be9c4c89da68105fd
Anil <Anil@anils-macbook.local>
2009-07-11 23:41:44 +0530

    Added xml, json api

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require 'rubygems'
 require 'sinatra'
+require 'builder'
+require 'json'
 
 $cheats_list = (`cheat sheets`).split("\n")
 
@@ -26,10 +28,10 @@ end
 
 get "/query/:cheat" do
 
-  current_cheat = params[:cheat]
+  current_cheat = params[:cheat].split(".")[0]
   @cookie = request.cookies["last_cheat"]
 
-  if current_cheat == @cookie
+  if (current_cheat == @cookie) and (current_cheat.nil? or current_cheat == "")
     redirect "/"
   end
 
@@ -37,10 +39,27 @@ get "/query/:cheat" do
 
   # Reset the cookie
   set_cookie("last_cheat", current_cheat)
-  erb :index
+  if params[:cheat].match(/(.xml)$/)
+    @cheat_name, @cheat_info = get_cheat_data(@cheats)
+    builder :index
+  elsif params[:cheat].match(/(.json)$/)
+    content_type 'application/json', :charset => 'utf-8'
+    @cheat_name, @cheat_info = get_cheat_data(@cheats)
+    response_json = {:cheat_name => @cheat_name, :cheat_info => @cheat_info}
+    response_json.to_json
+  else
+    erb :index
+  end
 
 end
 
+
+def get_cheat_data(cheats)
+  cheat_name = @cheats[0].gsub(":", "")
+  cheats.shift
+  cheat_info = cheats.join("<br />")
+  return cheat_name, cheat_info
+end
 
 #Exception handing for production environment
 configure :production do
---
+++ views/index.builder
@@ -0,0 +1,5 @@
+xml.instruct! :xml, :version => '1.1'
+xml.cheat do
+  xml.cheat_name @cheat_name
+  xml.cheat_info @cheat_info
+end
